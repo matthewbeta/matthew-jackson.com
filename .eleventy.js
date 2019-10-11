80a7c2462dc262d04f7a080c18e84b1ecc1b94b6
matthewbeta <hello@matthew-jackson.com>
2019-10-11 16:05:29 +0100

    Removes typekit font (for now), updates SW

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const moment = require('moment');
+const pluginPWA = require("eleventy-plugin-pwa")
 
 moment.locale('en');
 
@@ -23,6 +24,13 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({ "_netlify": "/" });
 
 	eleventyConfig.addPlugin(pluginRss);
+
+	eleventyConfig.addPlugin(pluginPWA, {
+    swDest: "./_site/service-worker.js",
+    globDirectory: "./_site",
+    clientsClaim: true,
+    skipWaiting: true
+  });
 
   return {
     passthroughFileCopy: true
--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,8 @@
-// ServiceWorker is a progressive technology. Ignore unsupported browsers
 if ("serviceWorker" in navigator) {
-  console.log("🤖 - Woohoo! Service workers!!! 🦄 ✨");
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(
-      function() {
-        console.log("🤖 - Service worker registration complete.");
-      },
-      function() {
-        console.log("🤖 - I fucked up. Soz");
-      }
-    );
-} else {
-  console.log("🤖 - Service workers are not supported 😭");
+	navigator.serviceWorker
+		.register("/service-worker.js")
+		.then(function() {
+			console.log("ServiceWorker has been registered!");
+		})
+		.catch(console.error);
 }
-
-(function () {
-var script = document.createElement('script');
-script.src = '/assets/fontfaceobserver.js';
-script.async = true;
-script.onload = function () {
-var franklin = new FontFaceObserver('franklin-gothic-urw', {weight: 400});
-var franklin_bold = new FontFaceObserver('franklin-gothic-urw', {weight: 500});
-Promise.all([franklin.load(), franklin_bold.load()]).then(function () {
-document.documentElement.classList.add('fonts-loaded');
-});
-};
-document.head.appendChild(script);
-}());
